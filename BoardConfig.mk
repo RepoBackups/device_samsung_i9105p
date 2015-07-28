19db56d81d2c83aad26292e9822a37f0cad83ee6
Hakam El Assouad <hakamelassouad@gmail.com>
2015-07-28 02:18:18 +0200

    Candy: add Wifi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,25 @@ BOARD_RIL_CLASS := ../../../device/samsung/galaxys2plus-common/ril/
 # NFC
 BOARD_NFC_HAL_SUFFIX := capri
 
+# Wi-Fi
+BOARD_HAVE_SAMSUNG_WIFI             := true
+BOARD_WLAN_DEVICE                   := bcmdhd
+BOARD_WLAN_DEVICE_REV               := bcm4330
+WPA_SUPPLICANT_VERSION              := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER         := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER                := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_MODULE_NAME             := "dhd"
+WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_PATH_PARAM           := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP              := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P             := "/system/etc/wifi/bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_STA             := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_MODULE_AP_ARG           := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                           := 802_11_ABG
+
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/samsung/i9105p/sepolicy
